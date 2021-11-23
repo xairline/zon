afa0868b463fc2e57ea0615c5de43f31c566ab28
Di Zou <ahoy@algol60.net>
2021-11-22 23:13:40 -0500

    feat: #8 show 2D landing movement on map

--- apps/ui/src/components/flight-log/stats/stats.tsx
+++ apps/ui/src/components/flight-log/stats/stats.tsx
@@ -1,7 +1,8 @@
 import { DualAxes } from '@ant-design/charts';
 import { LandingData, XPlaneData } from '@zon/xplane-data';
-import { Collapse, PageHeader } from 'antd';
+import { Col, Collapse, PageHeader, Row } from 'antd';
 import { useObserver } from 'mobx-react-lite';
+import ReactMapboxGl, { GeoJSONLayer, Source } from 'react-mapbox-gl';
 import React from 'react';
 const { Panel } = Collapse;
 /* eslint-disable-next-line */
@@ -12,17 +13,43 @@ export function Stats(props: StatsProps) {
     const mylandingData: LandingData = JSON.parse(
       localStorage.getItem('lastFlightLanding') || '{}'
     );
-    let touchDownTs = 0;
+    let touchDownIndex = 0;
     let index = 0;
     const mydata: any[] = [];
     const mydataTransformed: any[] = [];
-    mylandingData.data.forEach((data: any) => {
+    const geojson = {
+      type: 'FeatureCollection',
+      features: [
+        {
+          type: 'Feature',
+          properties: { color: '#F7455D', width: 3 },
+          geometry: {
+            coordinates: [],
+            type: 'LineString',
+          },
+        },
+        {
+          type: 'Feature',
+          properties: { color: '#e3c412', width: 5 },
+          geometry: {
+            coordinates: [],
+            type: 'LineString',
+          },
+        },
+      ],
+    };
+    for (const data of mylandingData.data) {
       if (data.ts === mylandingData.touchDown) {
-        touchDownTs = index;
+        touchDownIndex = index;
       }
       data.vs = XPlaneData.dataRoundup(data.vs * -1);
       data.agl = XPlaneData.dataRoundup(data.agl * 3.28084);
       data.ts = new Date(data.ts).toISOString();
+      if (touchDownIndex === 0) {
+        geojson.features[0].geometry.coordinates.push([data.lng, data.lat]);
+      } else {
+        geojson.features[1].geometry.coordinates.push([data.lng, data.lat]);
+      }
       if (data.agl < 50) {
         mydata.push(data);
         mydataTransformed.push({
@@ -42,7 +69,12 @@ export function Stats(props: StatsProps) {
         });
       }
       index++;
-    });
+      if (touchDownIndex !== 0 && index - 100 > touchDownIndex) {
+        break;
+      }
+    }
+    const lat = mylandingData.data[touchDownIndex].lat;
+    const lng = mylandingData.data[touchDownIndex].lng;
     const config = {
       data: [mydata, mydataTransformed],
       xField: 'ts',
@@ -56,15 +88,15 @@ export function Stats(props: StatsProps) {
           seriesField: 'name',
         },
       ],
-      height: 250,
+      height: 350,
       annotations:
-        touchDownTs !== 0
+        touchDownIndex !== 0
           ? [
               [
                 {
                   type: 'text',
                   position: {
-                    ts: mylandingData.data[touchDownTs].ts,
+                    ts: mylandingData.data[touchDownIndex].ts,
                     vs: 'max',
                   },
                   content: 'touch down',
@@ -73,12 +105,15 @@ export function Stats(props: StatsProps) {
                 {
                   type: 'region',
                   top: true,
-                  start: { ts: mylandingData.data[touchDownTs].ts, vs: 'min' },
+                  start: {
+                    ts: mylandingData.data[touchDownIndex].ts,
+                    vs: 'min',
+                  },
                   end: {
                     ts:
-                      touchDownTs + 1 === mylandingData.data.length
-                        ? mylandingData.data[touchDownTs].ts
-                        : mylandingData.data[touchDownTs + 1].ts,
+                      touchDownIndex + 1 === mylandingData.data.length
+                        ? mylandingData.data[touchDownIndex].ts
+                        : mylandingData.data[touchDownIndex + 1].ts,
                     vs: 'max',
                   },
                   style: {
@@ -98,6 +133,10 @@ export function Stats(props: StatsProps) {
         },
       },
     };
+    const Map = ReactMapboxGl({
+      accessToken:
+        'pk.eyJ1IjoieGFpcmxpbmUiLCJhIjoiY2t1OGg4YmlkNXZldzJvcG1ud3JwcDc0YiJ9.xLXSPf-vg2YZyjmIykUX-g',
+    });
     return (
       <PageHeader
         ghost={false}
@@ -111,10 +150,52 @@ export function Stats(props: StatsProps) {
         style={{
           width: '96%',
           marginLeft: '2%',
+          height: '100%',
         }}
         backIcon={false}
       >
-        <DualAxes {...config} />
+        <Row>
+          <Col span={12}>
+            <DualAxes {...config} />
+          </Col>
+          <Col span={12}>
+            <Map
+              style="mapbox://styles/mapbox/satellite-v9"
+              containerStyle={{
+                marginLeft: '16px',
+                height: '100%',
+                width: '100%',
+              }}
+              center={[lng, lat]}
+              zoom={[14.3]}
+              pitch={[53]}
+              bearing={[-23]}
+            >
+              <Source
+                id="mapbox-dem"
+                type="raster-dem"
+                url="mapbox://mapbox.mapbox-terrain-dem-v1"
+                tileSize={512}
+                maxzoom={14}
+              />
+              <GeoJSONLayer
+                data={geojson}
+                // linePaint={{
+                //   'line-color': 'red',
+                //   'line-width': 1,
+                // }}
+                lineLayout={{
+                  'line-cap': 'round',
+                  'line-join': 'round',
+                }}
+                linePaint={{
+                  'line-color': ['get', 'color'],
+                  'line-width': ['get', 'width'],
+                }}
+              />
+            </Map>
+          </Col>
+        </Row>
       </PageHeader>
     );
   });
